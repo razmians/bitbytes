abd931bcf108cc4c2fbad622185b2533a209ff05
Enayat Afridi <inayat.pcdc@gmail.com>
2018-12-20 20:28:08 +0500

    Drawer Added to the app

--- app/src/main/java/com/example/wasimabbas/ta/MainActivity.java
+++ app/src/main/java/com/example/wasimabbas/ta/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.wasimabbas.ta;
 
+import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()) {
+                    case R.id.dashboard_menu:
+                        // opens the main screen/dashboard
+                        Log.d(TAG, "onNavigationItemSelected: You clicked Dashboard");
+                        return true;
+
+                    case R.id.elements_menu:
+                        // Do something about elements
+                        Log.d(TAG, "onNavigationItemSelected: You clicked elements");
+                        return true;
+
+                    case R.id.notification_menu:
+                        //do something with notifications
+                        Log.d(TAG, "onNavigationItemSelected: You clicked notifications");
+                        return true;
+
+                    case R.id.profile_menu:
+                        // to open the user profile or registration form
+                        Log.d(TAG, "onNavigationItemSelected: You clicked profile");
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
